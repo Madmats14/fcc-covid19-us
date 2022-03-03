e5b623d3d3cbcbb8c865bead9203262952327c0f
Madmats14 <hd.cuevas@yahoo.com.ar>
2022-03-03 20:22:27 -0300

    navbar working, something wrong with styles

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,30 +1,35 @@
 <script>
+	let isOpen = false;
 	export let segment;
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
 
 <style>
-	@media (min-width: 769px) {
+	@media(min-width: 300px) {
 		nav {
-			border-bottom: solid rgb(250, 56, 56) 1px;
+			border-bottom: solid gray 5px;
 		}
 	}
 </style>
 
+
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
 		<a href="/" class="navbar-item">
-			<img src="logo.jpg" alt="virus" width="40" height="25"/>
+			<img src="logo.jpg" alt="virus"/>
 		</a>
-		<span class="navbar-burger"	aria-label="menu" aria-expanded="false">
+		<span class="navbar-burger" class:is-active={isOpen} on:click={toggleNav}	aria-label="menu" aria-expanded="false">
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
 		</span>
 	</div>
-	<div class="navbar-menu">
+	<div class="navbar-menu" class:is-active={isOpen}>
 		<div class="navbar-start">
-			<a href="/" class="navbar-item">Home</a>
-			<a href="/about" class="navbar-item">About</a>
+			<a href="/" class="navbar-item" class:is-active={segment === undefined }>Home</a>
+			<a href="/about" class="navbar-item" class:is-active={segment === 'about' }>About</a>
 		</div>
 	</div>
 </nav>
